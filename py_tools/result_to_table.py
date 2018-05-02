c7db4646411df81ecbf091f946ec7bfdc132d92b
dartoon <dingxuheng@mail.bnu.edu.cn>
2018-05-02 11:37:33 +0800

    save "%" in result_to_table.py

--- py_tools/result_to_table.py
+++ py_tools/result_to_table.py
@@ -12,10 +12,10 @@ import numpy as np
 
 result = input('data save in result.txt:\n')
 if 'qso_x' not in result:
-    print  result['host_mag'], '\t', result['n_sersic'],'\t', result['R_sersic'],'\t', result['host_flux_ratio_percent'],'\t', \
+    print  result['host_mag'], '\t', result['n_sersic'],'\t', result['R_sersic'],'\t', result['host_flux_ratio_percent'],'%','\t', \
 result['host_flux_ratio_percent'], '\t', result['redu_Chisq']
 elif 'qso_x' in result:
     dx = result['center_x']-result['qso_x']
     dy = result['center_y']-result['qso_y']
-    print result['host_mag'], '\t', result['n_sersic'],'\t', result['R_sersic'],'\t', result['host_flux_ratio_percent'],'\t', \
+    print result['host_mag'], '\t', result['n_sersic'],'\t', result['R_sersic'],'\t', result['host_flux_ratio_percent'],'%','\t', \
 result['host_flux_ratio_percent'], '\t', result['redu_Chisq'],'\t', dx, ',', dy
