correct PSF average median; re-runing PSF_color and testing PSF_median for four classic CID

analysis_Astrodrz/CID452/analysis/star_color.py
@@ -40,7 +40,7 @@ count=0
 psf_list_IR = copy.deepcopy(IR_psf_list[:-1])
 psf_list_IR = psf_list_IR[psf_list_IR[:,1].argsort()]
 flux_IR = np.zeros(len(psf_list_IR))
-flux_QSO_IR = 33.303   # Use the value as taking by plan b
+flux_QSO_IR = 33.037   # Use the value as taking by plan b
 for i in range(len(psf_list_IR)):
     PSF, center = cut_center_bright(image=img_IR, center=psf_list_IR[i], radius=60, return_center=True)
     region = pix_region(center=center, radius=12)    # take the radius = 12
@@ -64,7 +64,7 @@ count=0
 psf_list_acs = copy.deepcopy(acs_psf_list[:-1])
 psf_list_acs = psf_list_acs[psf_list_acs[:,0].argsort()]
 flux_acs = np.zeros(len(psf_list_acs))
-flux_QSO_acs = 28.84 # Use the value as taking by plan b.
+flux_QSO_acs = 29.208 # Use the value as taking by plan b.
 for i in range(len(psf_list_acs)):
     PSF, center = cut_center_bright(image=img_acs, center=psf_list_acs[i], radius=60, return_center=True)
     region = pix_region(center=center, radius=16)  # take the radius = 16
@@ -94,4 +94,4 @@ for i in range(len(PSF2QSO_IR)):
         mag_acs_corrs[i] = mag_acs[PSF_corrsp]
 QSO_mags = np.array([mag_QSO_IR,mag_QSO_acs])
 QSO_star_color(img_IR, QSO_pos_IR, QSO_mags, psf_list_IR, mag_IR, mag_acs_corrs,
-               mag_diff, ID=ID, reg_ty='astrodrz_06')
+               mag_diff, ID=ID, reg_ty='astrodrz_06', ifsave=True)

analysis_Astrodrz/CID70/analysis/star_color.py
@@ -40,7 +40,7 @@ count=0
 #psf_list_IR = copy.deepcopy(IR_psf_list[:-1])
 psf_list_IR = np.array([[2006,2058],[1334,1766],[1484,1674],[1002,1484],[2388,1784],[2140,1522]])
 flux_IR = np.zeros(len(psf_list_IR))
-flux_QSO_IR = 297.698   # Use the value as taking by plan b
+flux_QSO_IR = 297.456   # Use the value as taking by plan b
 for i in range(len(psf_list_IR)):
     PSF, center = cut_center_bright(image=img_IR, center=psf_list_IR[i], radius=60, return_center=True)
     region = pix_region(center=center, radius=12)    # take the radius = 12
@@ -100,4 +100,4 @@ for i in range(len(PSF2QSO_IR)):
         mag_acs_corrs[i] = mag_acs[PSF_corrsp]
 QSO_mags = np.array([mag_QSO_IR,mag_QSO_acs])
 QSO_star_color(img_IR, QSO_pos_IR, QSO_mags, psf_list_IR, mag_IR, mag_acs_corrs,
-               mag_diff, ID=ID, reg_ty='astrodrz_06')
+               mag_diff, ID=ID, reg_ty='astrodrz_06', ifsave=True)

py_tools/psfs_average.py
@@ -121,14 +121,21 @@ def psf_ave(psfs_list, not_count=(), mode = 'direct',  mask_list=['default.reg']
         psf_std = psf_std.data
         psf_ave = psf_ave.data
     elif mode == 'mid':
+        weights = np.zeros(psf_NO)        
+        for i in range(psf_NO):
+            box_c = len(psfs_l_msk[i])/2
+            box_r = len(psfs_l_msk[i])/6
+            if psfs_l_msk[i].sum() != 0:
+                weights[i] = np.sqrt(np.sum(psfs_l_msk[i][box_c-box_r:box_c+box_r,box_c-box_r:box_c+box_r]))  # set weight based on their intensity (SNR)
+                psfs_l_msk[i] /= weights[i] **2  # scale the image to a same level
         sz = len(psfs_l_msk[0])
         psf_ave = np.zeros_like(psfs_l_msk[0])
-        plt.imshow(psfs_l_msk[1],norm=LogNorm())
         for i in range(sz):
             for j in range(sz):
                 psf_cell = psfs_l_msk[:,i,j]
                 psf_cell = psf_cell[psf_cell!=0.]  # Delete the non-zeros.
                 psf_ave[i,j] = median(psf_cell)
+        psf_ave /= psf_ave.sum()        
     #### The PSF are found not very necessary to be shiftted. !!!! Note the high_CI is not ready --- high_res. mask is not OK.
 #    if mode == 'high_CI':
 #        psfs_high_list = np.empty([psf_NO, psfs_list[0].shape[0]*scale, psfs_list[0].shape[1]*scale])