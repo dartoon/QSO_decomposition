937b6e72309a60a9c77753ec6556eb3bb871bc7d
dartoon <dingxuheng@mail.bnu.edu.cn>
2018-07-24 14:23:08 +0900

    Fit samples

---
+++ analysis_Astrodrz/CID452/analysis/esti_img_bkg.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 13 13:40:56 2018
+
+@author: Dartoon
+
+test the flux profile for QSO
+"""
+
+import numpy as np
+import sys
+sys.path.insert(0,'../../../py_tools')
+import astropy.io.fits as pyfits
+from flux_profile import flux_profile, SB_profile, min_sub
+import glob
+
+ID='CID452'
+
+test_n=20
+img = pyfits.getdata('{0}_cutout.fits'.format(ID))
+center = np.asarray(img.shape) /2
+mask_list = glob.glob("QSO_msk*.reg")   # Read *.reg files in a list.
+sub, value = min_sub(0.001, img, mask_list=mask_list, test_n=test_n)
+r_flux, r_grids, regions=flux_profile(img-sub, center,radius=center.min(), grids=50, ifplot=True, fits_plot= True, mask_list=mask_list)
+#print r_flux[-test_n:],'\n',r_flux[-test_n:][1:]-r_flux[-test_n:][:-1], '\n', 
+print sub
+print (r_flux[-test_n:][1:]-r_flux[-test_n:][:-1]).sum(), value
+
+#sub_list = np.zeros(7)
+#value = np.zeros(7)
+#for i in range(7):
+#    print 'PSF{0}'.format(i)
+#    img = pyfits.getdata('PSF{0}.fits'.format(i))
+#    center = np.asarray(img.shape) /2
+#    mask_list = glob.glob("PSF{0}*.reg".format(i))   # Read *.reg files in a list.
+#    sub_list[i], value[i] = min_sub(0.001, img, mask_list=mask_list)
+#    print sub_list[i]
+#    r_flux, r_grids, regions=flux_profile(img- sub_list[i], center,
+#                                          radius=center.min(), grids=50,
+#                                          ifplot=True, fits_plot= True,
+#                                          mask_list=mask_list)
+##    print r_flux[-test_n:],'\n',r_flux[-test_n:][1:]-r_flux[-test_n:][:-1], '\n'
+##    print (r_flux[-test_n:][1:]-r_flux[-test_n:][:-1]).sum()
+#print sub_list, value
---
+++ analysis_Astrodrz/CID70/analysis/cal_Err.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 19 12:27:43 2018
+
+@author: dartoon
+
+Calculate the noise map level from the undrizzled image.
+"""
+import astropy.io.fits as pyfits
+import numpy as np
+import matplotlib.pylab as plt
+#import glob
+#from matplotlib.colors import LogNorm
+
+ID = 'CID70'
+
+wht = pyfits.getdata('wht_map.fits')
+exp = 2400.
+mean_wht = 2395.399 * (0.0642/0.135)**2
+exp_map = exp * wht/mean_wht
+
+
+stdd = 0.0076
+filename = '{0}_cutout.fits'.format(ID)
+image = pyfits.open(filename)
+flux = image[0].data.copy()
+rms =(flux/exp_map+stdd**2)**0.5
+pyfits.PrimaryHDU(exp_map).writeto('exp_map.fits',overwrite=True) 
+pyfits.PrimaryHDU(rms).writeto('wht_err.fits',overwrite=True) 
