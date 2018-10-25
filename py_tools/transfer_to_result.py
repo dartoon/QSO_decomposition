dfc90415e24487f684e38ad9124372ae7450f3b4
dartoon <dingxuheng@mail.bnu.edu.cn>
2018-10-25 09:52:48 -0700

    record the Chisq information for only PS fitting

--- py_tools/transfer_to_result.py
+++ py_tools/transfer_to_result.py
@@ -87,3 +87,52 @@ def transfer_to_result(data, source_result, ps_result, image_ps, image_host,
     result['redu_Chisq'] = round(reduced_Chisq,6)
     
     return result
+
+def transfer_to_result_ps(data, ps_result, image_ps, image_host,
+                       error_map, filt, fixcenter,ID, cut=0, plot_compare=True,
+                       QSO_msk_list = "",pix_sz = 'drz06', QSO_msk = None,
+                       tag=None):
+    #==============================================================================
+    # Translate the e1, e2 to phi_G and q
+    #==============================================================================
+    #==============================================================================
+    # Save the result
+    #==============================================================================
+    from roundme import roundme
+    result = {}
+    ##==============================================================================
+    ##Plot the images for adopting in the paper
+    ##==============================================================================
+    from flux_profile import total_compare
+    data = data
+    QSO = image_ps
+    if QSO_msk is None:
+        QSO_mask = data*0 + 1
+    else:
+        QSO_mask = QSO_msk
+    if len(image_host) == 0:
+        host = data*0
+        label = ['data', 'QSO', 'host=0', 'model', 'normalized residual']
+    elif len(image_host) >1:
+        host = np.zeros_like(image_host[0])
+        for i in range(len(image_host)):
+            host += image_host[i]
+        label = ['data', 'QSO', '{0}objs'.format(i), 'model', 'normalized residual']  #Print the numbers of objects
+    flux_list = [data, QSO, host, error_map]
+    import glob
+    mask_list = glob.glob(QSO_msk_list)   # Read *.reg files in a list.
+#    print "mask_list,muhahah", mask_list
+    fig = total_compare(label_list = label, flux_list = flux_list, target_ID = ID, pix_sz=pix_sz,
+                  data_mask_list = mask_list, data_cut = cut, facility = filt,
+                  plot_compare = plot_compare, msk_image = QSO_mask)
+    if tag is not None:
+        fig.savefig("{0}_SB_profile.pdf".format(tag), bbox_inches = 'tight')
+    # =============================================================================
+    # Calculate reduced Chisq and save to result
+    # =============================================================================
+    chiq_map = ((data-image_ps-host)/(error_map))**2 * QSO_mask
+    pixels=len(error_map)**2 - (1-QSO_mask).sum()
+    reduced_Chisq = chiq_map.sum()/pixels
+    result=roundme(result)
+    result['redu_Chisq'] = round(reduced_Chisq,6)
+    return result
