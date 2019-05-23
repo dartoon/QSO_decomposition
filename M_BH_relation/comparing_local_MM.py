e6827c446189b03385f2fa4424df37d26645b24e
dartoon <dingxuheng@mail.bnu.edu.cn>
2019-05-22 19:14:39 -0700

    Update comparing_local_MM.py

--- M_BH_relation/comparing_local_MM.py
+++ M_BH_relation/comparing_local_MM.py
@@ -44,7 +44,7 @@ kor_e[:,2]= Kore_loc[:,2]  # error
 kor_e[:,3]= np.log10(Kore_loc[:,3]) + Kore_loc[:,6]     #LgMBH
 kor_e[:,4]= np.sqrt((np.log10(Kore_loc[:,3]) - np.log10(Kore_loc[:,4]))**2 +  (np.log10(Kore_loc[:,5]) - np.log10(Kore_loc[:,3]))**2) # error
 kor_e[:,4][kor_e[:,4]==np.inf] =  (np.log10(Kore_loc[:,5]) - np.log10(Kore_loc[:,3]))[kor_e[:,4]==np.inf]
-plt.errorbar(kor_e[:,1],kor_e[:,3], xerr=kor_e[:,2] ,yerr=kor_e[:,4],fmt='.',color='red',markersize=15, label='Kormendy2013 elliptical')
+plt.errorbar(kor_e[:,1],kor_e[:,3], xerr=kor_e[:,2] ,yerr=kor_e[:,4],fmt='.',color='red',markersize=15, label='Kormendy 2013 elliptical')
 
 #%%########input local classic bulge by Kormendy ############
 #Distance(Mpc) logMbulge error MBH low high order flag(0/reg, 1/1, 2/2)
@@ -58,7 +58,7 @@ kocb_e[:,2]= Kocb_loc[:,2]  # error
 kocb_e[:,3]= np.log10(Kocb_loc[:,3]) + Kocb_loc[:,6]     #LgMBH
 kocb_e[:,4]= np.sqrt((np.log10(Kocb_loc[:,3]) - np.log10(Kocb_loc[:,4]))**2 +  (np.log10(Kocb_loc[:,5]) - np.log10(Kocb_loc[:,3]))**2) # error
 plt.errorbar(kocb_e[:,1],kocb_e[:,3], xerr=kocb_e[:,2] ,
-             yerr=kocb_e[:,4],fmt='.',color='blue',markersize=15, label='Kormendy2013 classical bulge')
+             yerr=kocb_e[:,4],fmt='.',color='blue',markersize=15, label='Kormendy 2013 classical bulge')
 
 
 #############################################################
@@ -80,7 +80,7 @@ nll = lambda *args: -lnlike(*args)
 result = op.minimize(nll, [0.93027905, -1.95536508, 0.35], args=(x0, y0, yerr0))
 m_ml0, b_ml0, sint_ml0= result["x"]
 xl = np.linspace(5, 13, 100)
-#plt.plot(xl, m_ml0*xl+b_ml0, color="k", linewidth=4.0,zorder=-0.5)
+plt.plot(xl, m_ml0*xl+b_ml0, color="k", linewidth=4.0,zorder=-0.5)
 
 ###################fitting f1 f3 f4 with MCMC#########################
 #np.concatenate((z_ss, z_b11, z_cosmos),axis=0)
@@ -93,19 +93,20 @@ yerr1=(np.concatenate((bloc[:,2], kor_e[:,2], kocb_e[:,2]),axis=0)**2+np.concate
 nll = lambda *args: -lnlike(*args)
 result = op.minimize(nll, [0.93027905, -1.95536508, 0.35], args=(x1, y1, yerr1))
 m_ml1, b_ml1,sint_ml1= result["x"]
-#plt.plot(xl, m_ml1*xl+b_ml1, color="red", linewidth=4.0,zorder=-0.5)
+plt.plot(xl, m_ml1*xl+b_ml1, color="red", linewidth=4.0,zorder=-0.5)
 
 
-#plt.text(9.3, 6.0, "log$(M_{BH}/10^{7}M_{\odot})$=%s+%slog$(M_*/10^{10}M_{\odot})$"%(round(b_ml0+m_ml0*10-7,2),round(m_ml0,2)),color='black',fontsize=25)
-#plt.text(9.3, 6.25, "log$(M_{BH}/10^{7}M_{\odot})$=%s+%slog$(M_*/10^{10}M_{\odot})$"%(round(b_ml1+m_ml1*10-7,2),round(m_ml1,2)),color='red',fontsize=25)
+plt.text(10.5, 6.5, "log$(M_{BH}/10^{7}M_{\odot})$=%s+%slog$(M_*/10^{10}M_{\odot})$"%(round(b_ml0+m_ml0*10-7,2),round(m_ml0,2)),color='black',fontsize=20)
+plt.text(9.20, 9.5, "log$(M_{BH}/10^{7}M_{\odot})$=%s+%slog$(M_*/10^{10}M_{\odot})$"%(round(b_ml1+m_ml1*10-7,2),round(m_ml1,2)),color='red',fontsize=20)
 
 
 plt.xlim(9,12.5)
 plt.ylim(6.0,11)
-plt.title("Comparsion of the $M_{BH}$ - $M_*$ relation at local universe",fontsize=25)
+#plt.title("Comparsion of the $M_{BH}$ - $M_*$ relation at local universe",fontsize=25)
 plt.xlabel("log$(M_*/M_{\odot})$",fontsize=35)
 plt.ylabel("log$(M_{BH}/M_{\odot})$",fontsize=35)
 plt.grid(linestyle='--')
 plt.tick_params(labelsize=25)
 plt.legend(prop={'size':20},loc=2)
+#plt.savefig("local_MM_comparison.pdf")
 plt.show()
